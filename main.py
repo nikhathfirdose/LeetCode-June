0e41a423e1e6fd009575fbe2b74e9a85491e0016
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-06-13 12:40:58 +0000

    flatten - 12.1

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
-# Day12
-#Design a data structure that supports all following operations in average O(1) time.
+iceCream = 100
+queue = ["A","C","O","A"]
+dic ={"A": 5, "C": 7, "O": 2}
 
-# insert(val): Inserts an item val to the set if not already present.
-# remove(val): Removes an item val from the set if present.
-# getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
-from random import choice
-class RandomizedSet():
-    def __init__(self):
-        self.dictionary = {}
-        self.Array = []
+while dic[queue[0]]<=iceCream:
+  first = queue.pop(0)
+  iceCream-= dic[first]
+  queue.append(first)
+  print(queue)
+  print(iceCream)
 
-        
-    def insert(self, val: int) -> bool:
-        if val in self.dictionary:
-            return False
-        self.dictionary[val] = len(self.Array)
-        self.Array.append(val)
-        return True
-        
+def flatten(S):
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
+s=[[1,2],[3,4],[1,2,3,[9,6,7]]]
+print(flatten(s))
+  
 
-    def remove(self, val: int) -> bool:
-        if val in self.dictionary:
-            last_element, idx_of_value_to_be_delted = self.Array[-1], self.dictionary[val]
-            self.Array[idx_of_value_to_be_delted], self.dictionary[last_element] = last_element, idx_of_value_to_be_delted
-            self.Array.pop()
-            del self.dictionary[val]
-            return True
-        return False
-
-    def getRandom(self) -> int:
-        return choice(self.Array)
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
